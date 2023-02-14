d3040a060b2e9ca1dfa4daf82eabaeaa69c0f7ec
crewcrew23 <vasiliyiordanov23@gmail.com>
2023-02-14 18:21:27 +0000

    add favicon

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header_nav_active');
     });
+    console.log(1);
     if (window.innerWidth <= 767) {
         for (let i = 0; i < menuLinks.length; i += 1) {
             menuLinks[i].addEventListener('click', () => {
